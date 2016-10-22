48d50f6495492ada36aa8648940ff72ad9daef46
Natix <natix643@gmail.com>
2016-10-22 02:00:52 +0200

    client-simple buildscript

---
+++ client-simple/src/main/java/credit/client/SimpleClient.java
@@ -0,0 +1,14 @@
+package credit.client;
+
+import credit.server.Debtor;
+
+public class SimpleClient {
+
+    public void useServerClass() {
+        Debtor debtor = new Debtor();
+        debtor.setFirstName("Jiří");
+        debtor.setLastName("Machart");
+        // etc...
+    }
+
+}
